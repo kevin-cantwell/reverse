da91b29602b4537b726c8b8f10d23ab8e938834f
Kevin Cantwell <kevin.cantwell@gmail.com>
2017-03-29 10:09:46 -0400

    adds ReadForward and updates docs

--- reverse.go
+++ reverse.go
@@ -1,5 +1,23 @@
 /*
  Package reverse is a proof of concept package which implements a reverse io.Reader.
+
+ In order to efficiently read a byte stream in reverse order, we must have access to
+ a io.Seeker. Almost certainly this will be an instance of *os.File, but this package only
+ requires interfaces. The interface declared here is a ReadAtSeeker (io.ReaderAt + io.Seeker).
+ The *os.File type implements ReadAt and Seek, which is handy, but not many other types do.
+
+ This package could also have been designed using a more basic combination of just Seek and
+ Read. However, ReadAt is useful in that it doesn't change the internal seek position when
+ invoked whereas Read does.
+
+ Before reading in reverse, the seek position must be changed from 0, otherwise Read will
+ return immediately with an io.EOF error (since we're reading tail-to-head). This can be
+ done before instantiating a *Reader by invoking Seek, or after instantiating the reader
+ by invoking the convenience method SeekToEnd.
+
+ All calls to Read will read bytes in reverse order, updating the underlying seek offset.
+ Bytes can be read in a forwards direction by invoking ReadForward, which is similar to
+ a normal Read on the underlying ReadAtSeeker.
 */
 package reverse
 
@@ -24,11 +42,18 @@ func NewReader(ras ReadAtSeeker) *Reader {
 	}
 }
 
-// SeekToEnd is a convenience function to move the offset at the end of the underlying io.Seeker.
-// For purposes of reading from some other offset, invoke the underlying io.Seeker. It returns
-// the new offset and any error that occurred. The contract is identical to io.Seeker#Seek.
+// SeekToEnd is a convenience function to move the offset to the end of the underlying ReadAtSeeker.
+// For purposes of reading from some other offset, invoke the underlying ReadAtSeeker. It returns
+// the new offset and any error that occurred. The contract is identical to io.Seeker#Seek(0, io.SeekEnd).
 func (r *Reader) SeekToEnd() (int64, error) {
 	return r.ras.Seek(0, io.SeekEnd)
+}
+
+// SeekToBeginning is a convenience function to move the offset to the beginning of the underlying
+// ReadAtSeeker. It returns the new offset and any error that occurred. The contract is identical
+// to io.Seeker#Seek(0, io.SeekStart).
+func (r *Reader) SeekToStart() (int64, error) {
+	return r.ras.Seek(0, io.SeekStart)
 }
 
 /*
@@ -65,4 +90,24 @@ func (r *Reader) Read(b []byte) (int, error) {
 		m += n
 	}
 	return m, nil
+}
+
+/*
+  ReadForward is the opposite of Read. Bytes are read in the forward direction.
+  It returns the number of bytes read and any error encounted. Read and ReadForward
+  may be used in combination to reset the underlying seek position to where it started.
+*/
+func (r *Reader) ReadForward(b []byte) (int, error) {
+	offset, err := r.ras.Seek(0, io.SeekCurrent)
+	if err != nil {
+		return 0, err
+	}
+
+	// Make sure to update the seek position, even on errors
+
+	n, err := r.ras.ReadAt(b, offset)
+	if n != 0 {
+		r.ras.Seek(int64(n), io.SeekCurrent)
+	}
+	return n, err
 }
--- reverse_test.go
+++ reverse_test.go
@@ -2,6 +2,7 @@ package reverse_test
 
 import (
 	"bufio"
+	"io"
 	"io/ioutil"
 	"os"
 	"strings"
@@ -12,30 +13,62 @@ import (
 )
 
 var _ = Describe("Reader", func() {
-	It("Should read in reverse", func() {
-		file, info, err := createFileFromLines(
-			"foo",
-			"bar",
-			"baz",
-		)
-		defer file.Close()
+	Context("#Read", func() {
+		It("Should read in reverse", func() {
+			file, info, err := createFileFromLines(
+				"foo",
+				"bar",
+				"baz",
+			)
+			defer file.Close()
 
-		rev := reverse.NewReader(file)
-		offset, err := rev.SeekToEnd()
+			rev := reverse.NewReader(file)
+			offset, err := rev.SeekToEnd()
 
-		Expect(err).To(BeNil())
-		Expect(offset).To(Equal(info.Size()))
+			Expect(err).To(BeNil())
+			Expect(offset).To(Equal(info.Size()))
 
-		// Now we can use a regular scanner to scan lines in reverse!
-		scanner := bufio.NewScanner(rev)
-		scanner.Scan()
-		Expect(scanner.Text()).To(Equal("zab"))
-		scanner.Scan()
-		Expect(scanner.Text()).To(Equal("rab"))
-		scanner.Scan()
-		Expect(scanner.Text()).To(Equal("oof"))
+			// Now we can use a regular scanner to scan lines in reverse!
+			scanner := bufio.NewScanner(rev)
+			scanner.Scan()
+			Expect(scanner.Text()).To(Equal("zab"))
+			scanner.Scan()
+			Expect(scanner.Text()).To(Equal("rab"))
+			scanner.Scan()
+			Expect(scanner.Text()).To(Equal("oof"))
 
-		Expect(scanner.Err()).To(BeNil())
+			Expect(scanner.Err()).To(BeNil())
+		})
+	})
+	Context("#ReadForward", func() {
+		It("Should read forward", func() {
+			file, _, _ := createFileFromLines(
+				"foo",
+				"bar",
+				"baz",
+			)
+			defer file.Close()
+
+			rev := reverse.NewReader(file)
+
+			foo := make([]byte, 4)
+			n, err := rev.ReadForward(foo)
+			Expect(err).To(BeNil())
+			Expect(n).To(Equal(4))
+			Expect(string(foo)).To(Equal("foo\n"))
+
+			bar := make([]byte, 4)
+			n, err = rev.ReadForward(bar)
+			Expect(err).To(BeNil())
+			Expect(n).To(Equal(4))
+			Expect(string(bar)).To(Equal("bar\n"))
+
+			baz := make([]byte, 4)
+			n, err = rev.ReadForward(baz)
+			Expect(err).To(Equal(io.EOF))
+			Expect(n).To(Equal(3))
+			Expect(string(baz[0:3])).To(Equal("baz"))
+		})
 	})
 })
 
@@ -46,6 +79,8 @@ func createFileFromLines(data ...string) (*os.File, os.FileInfo, error) {
 	}
 
 	_, err = tmp.Write([]byte(strings.Join(data, "\n")))
+	tmp.Close()
+	tmp, err = os.Open(tmp.Name()) // Re-open it so the seeker is not at "end" after writing
 
 	info, err := tmp.Stat()
 	if err != nil {
